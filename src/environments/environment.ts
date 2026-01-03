6a0ca27a50c14e4bd36c78ad2dc9b2ba5211dc71
Ammar <abgoooor201596@gmail.com>
2026-01-03 20:10:24 +0300

    Add Google OAuth Client ID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,8 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api',
   version: '1.0.0',
-  // Google OAuth Client ID - Get from Google Cloud Console
-  // https://console.cloud.google.com/apis/credentials
-  googleClientId: '', // Add your Google Client ID here
+  // Google OAuth Client ID
+  googleClientId: '826988304508-nb0662tfl3uo5b0b87tmvkvmsbtn6g44.apps.googleusercontent.com',
   features: {
     offlineMode: true,
     analytics: false,
